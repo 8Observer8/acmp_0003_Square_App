9fcf89366bc7fcc4608ac42776bc4081b277f280
Ivan <8observer8@gmail.com>
2013-05-12 19:06:43 +0400

    There are stub, tests and documentation.

---
+++ src/acmp_0003_square_app/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package acmp_0003_square_app;
+
+import org.me.square.Square;
+
+/**
+ *
+ * @author Ivan
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int number = 5;
+        int result = Square.square(number);
+        System.out.println("Result = " + result);
+    }
+}
